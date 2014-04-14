76e6411a9223808b68ce3a18295d8c9c83b2c91f
Natalia Bartol <natalia.b@zend.com>
2014-04-14 17:18:03 +0100

    ZSTD-4054 Studio will not notify about maintenance (minor) update if major update is also available but license is not valid
    Signed-off-by: Natalia Bartol <natalia.b@zend.com>

--- bundles/org.eclipse.equinox.p2.director/src/org/eclipse/equinox/p2/planner/PlanVerifier.java
+++ bundles/org.eclipse.equinox.p2.director/src/org/eclipse/equinox/p2/planner/PlanVerifier.java
@@ -1,12 +1,19 @@
 package org.eclipse.equinox.p2.planner;
 
+import java.util.Map;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.equinox.p2.engine.IProvisioningPlan;
+import org.eclipse.equinox.p2.metadata.IInstallableUnit;
 
 public abstract class PlanVerifier {
 	/**
 	 * Verifies provisioning plan from P2 solver
 	 */
 	public abstract IStatus verify(IProvisioningPlan plan);
+
+	/**
+	 * Verifies possible updates
+	 */
+	public abstract Map<String, IInstallableUnit> verifyUpdates(Map<String, IInstallableUnit> in);
 
 }
